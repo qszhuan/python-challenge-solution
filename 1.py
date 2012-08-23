f644d6607f01d71868986fa7507489bb8502ac30
zhuanqs <zhuanqingshan@163.com>
2012-08-23 22:11:50 +0800

    level 1: use const of string module

--- 1.py
+++ 1.py
@@ -1,9 +1,10 @@
 #http://www.pythonchallenge.com/pc/def/map.html
-from string import maketrans, translate
+from string import maketrans, translate, ascii_lowercase
 
 text = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
 
-table =  maketrans('abcdefghijklmnopqrstuvwxyz', 'cdefghijklmnopqrstuvwxyzab')
+#table =  maketrans('abcdefghijklmnopqrstuvwxyz', 'cdefghijklmnopqrstuvwxyzab')
+table = maketrans(ascii_lowercase, ascii_lowercase[2:]+ascii_lowercase[:2])
 result = text.translate(table)
 print result
 
