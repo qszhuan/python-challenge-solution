ab07280a76bea0fb1d2018248e16c9aa96405a38
zhuanqs <zhuanqingshan@163.com>
2012-08-24 23:42:37 +0800

    level 10

--- 10.py
+++ 10.py
@@ -3,3 +3,15 @@
 # a = [1, 11, 21, 1211, 111221, 
 # len(a[30]) = ?
 
+index = 0
+start = '1'
+while index <= 30:
+    print index, len(start)
+    next = []
+    for each in start:
+        if next and  next[-1][0] == each:
+            next[-1][1] = next[-1][1] + 1
+        else:
+            next.append([each, 1])
+    start =  ''.join(str(count)+str(num) for num, count in next)
+    index = index + 1    
