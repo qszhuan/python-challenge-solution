ea94ad3cc0ae7ecbc90a1660d5733394905cdc47
zhuanqs <zhuanqingshan@163.com>
2012-08-29 23:34:29 +0800

    level 18

--- 18.py
+++ 18.py
@@ -7,6 +7,7 @@ import urllib
 
 #urllib.urlretrieve('http://huge:file@www.pythonchallenge.com/pc/return/deltas.gz','deltas.gz')
 
+# get two columns
 import gzip
 
 gz_file = gzip.GzipFile('deltas.gz')
@@ -14,15 +15,15 @@ lines = gz_file.read().split('\n')
 pairs =  [line.rpartition('   ')[::2] for line in lines]
 columns = [[p[i] for p in pairs] for i in range(2)]
 
+#find the difference
 import difflib
-diffs = difflib.Differ().compare(columns[0], columns[1])
+diffs = list(difflib.Differ().compare(columns[0], columns[1]))
 import pprint
 pprint.pprint(list(diffs))
-
 pngs = [''.join(filter(lambda l: l[0] == d, diffs)) for d in " -+"]
-print pngs
+
+#output image
 import codecs, re
-def unhex(s): 
-    return codecs.getdecoder('hex')(re.sub('[^0-9a-fA-F]', '', s))[0]
+def unhex(s): return codecs.getdecoder('hex')(re.sub('[^0-9a-fA-F]', '', s))[0]
 
 for i in range(len(pngs)): open('delta%d.png' % (i + 2), 'wb').write(unhex(pngs[i]))
