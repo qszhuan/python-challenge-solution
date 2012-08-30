4ddffa278c65da7187a5ad53304869409b494161
zhuanqs <zhuanqingshan@163.com>
2012-08-31 00:48:37 +0800

    level 19:wave

--- 19.py
+++ 19.py
@@ -17,3 +17,16 @@ import base64
 dec = base64.decodestring(encoded)
 f = open('indian.wav', 'wb')
 f.write(dec)
+
+import wave
+iw = wave.open('indian.wav')
+frames = iw.readframes(iw.getnframes())
+print frames[:40]
+
+iw2 = wave.open('indian2.wav', 'w')
+iw2.setnchannels(1)
+iw2.setsampwidth(iw.getsampwidth())
+iw2.setframerate(iw.getframerate()//2)
+iw2.writeframes(frames[::2])
+iw2.close()
+iw.close()
