22873163a52bf5525a31e55e8d369a4b0875ca82
zhuanqs <zhuanqingshan@163.com>
2012-09-01 23:58:50 +0800

    level 21: path

--- 21/21.py
+++ 21/21.py
@@ -1,2 +1,4 @@
-#! /Users/zhuanqingshan/virtual_env/datawinners/bin python
-print open('readme.txt').read()
+#! /Users/zhuanqingshan/virtual_env/datawinners/bin/python
+import os, sys
+cur_file_dir= sys.path[0]
+print open(os.path.join(cur_file_dir, 'readme.txt')).read()
