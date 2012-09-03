430d949563382864b24c1dcac591ecc114185f9a
zhuanqs <zhuanqingshan@163.com>
2012-09-03 23:44:27 +0800

    level 21: zlib

--- 21/21.py
+++ 21/21.py
@@ -2,3 +2,13 @@
 import os, sys
 cur_file_dir= sys.path[0]
 print open(os.path.join(cur_file_dir, 'readme.txt')).read()
+
+f = open(os.path.join(cur_file_dir, 'package.pack'))
+data = f.read()
+
+import codecs
+print codecs.encode(data[:20], 'hex')
+
+import zlib
+sample = zlib.decompress(data)[:20]
+
